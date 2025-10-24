d376103e620e2e78edffb88f754208cd404e2e98
Shivam <shivam@appentus.in>
2025-10-24 13:14:51 +0530

    update code

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,22 +1,13 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
-import 'package:my_reminder/app/modules/bindings/home_binding.dart';
-import 'package:my_reminder/app/modules/views/home_view.dart';
-import 'package:my_reminder/app/routes/app_routes.dart';
+
+import '../modules/bindings/home_binding.dart';
+import '../modules/views/home_view.dart';
+import 'app_routes.dart';
 
 class AppPages {
   static const initial = Routes.home;
 
   static final routes = [
-    GetPage(
-      name: Routes.home,
-      page: () =>  HomeView(),
-      binding: HomeBinding(),
-    ),
-    // GetPage(
-    //   name: Routes.login,
-    //   page: () => const LoginView(),
-    //   binding: LoginBinding(),
-    // ),
+    GetPage(name: Routes.home, page: () => HomeView(), binding: HomeBinding()),
   ];
 }
-
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,4 +1,3 @@
 abstract class Routes {
   static const home = '/home';
-  static const login = '/login';
 }
